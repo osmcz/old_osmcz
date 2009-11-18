4657fc332c57046c4b26f2bab2ab97202f45ca5e
walley <walley@818622b3-d97c-4610-bfa0-62a035a5a0f4>
2009-11-18 20:19:44 +0000

    move the file to new location

--- exifme.pl
+++ exifme.pl
@@ -1,6 +1,7 @@
 #!/usr/bin/perl
 use DBI;
-
+use File::Basename;
+use File::Copy;
 
 
 #0x0001|N                                                                       
@@ -14,9 +15,9 @@ use DBI;
 # insert into guidepost values (NULL, 50.1, 17.1, 'x', 'znacka');
 
 print "start\n";
-system ("id");
 $i = $ARGV[0];
 $author = $ARGV[1];
+$new_location = $ARGV[2];
 @output = `exif -i $i`;
 @exiflat = grep(/^0x0002/, @output);
 @exiflon = grep(/^0x0004/, @output);
@@ -33,10 +34,10 @@ print "$lat $lon\n";
 my $dbfile = 'guidepost';
 my $dbh = DBI->connect("dbi:SQLite2:dbname=$dbfile","","",{ RaiseError => 1 },) or die $DBI::errstr;
 print"cont";
-$q = "insert into guidepost values (NULL, $lat, $lon, 'img/guidepost/".$i."','".$i."', '$author');\n";
+$q = "insert into guidepost values (NULL, $lat, $lon, '".$i."','".basename($i)."', '$author');\n";
 print $q;
 $dbh->do($q) or die;
 print "sql executed";
 $dbh->disconnect();
-
+move(basename($i),$new_location);
 print "done";
