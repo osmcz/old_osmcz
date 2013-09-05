9a8eadb7b432c073c4db75d1eff7373cad1cc0ec
walley <walley@818622b3-d97c-4610-bfa0-62a035a5a0f4>
2013-09-05 14:53:27 +0000

    editor

--- osmcz/p/Guidepost/Table.pm
+++ osmcz/p/Guidepost/Table.pm
@@ -3,28 +3,55 @@ package Guidepost::Table;
 use Apache2::Reload;
 use Apache2::RequestRec ();
 use Apache2::RequestIO ();
+use Apache2::URI ();
+use APR::URI ();
+
+use APR::Const -compile => qw(URI_UNP_REVEALPASSWORD);
 use Apache2::Const -compile => qw(OK);
+
 use DBI;
 
 
-  sub handler {
-    my($r) = @_;    # grab the request info "object" that mod_perl has
+sub handler {
+  my($r) = @_;    # grab the request info "object" that mod_perl has
 #      my $r = shift;
-    $r->content_type('text/html');
+  $r->content_type('text/html');
 
 #    %ENV  = $r->cgi_env;
+  my $uri = $r->uri;      # what does the URI (URL) look like ?
+  print $uri;
 
+  my $query_string = $r->args;
+  $r->print( "Hello world! Your query string was: $query_string\n" );
 
-    my $uri = $r->uri;      # what does the URI (URL) look like ?
-print $uri;
+  if ($uri =~ "table\/all") {
+    print "<h1>all</h1>\n";
+  } elsif ($uri =~ /goodbye/) {
+    say_goodbye($r);
+  } elsif ($uri =~ "/table/get") {
 
-    my $query_string = $r->args;
-    $r->print( "Hello world! Your query string was: $query_string\n" );
-    if ($uri =~ "table\/all") {
-print "<h1>all</h1>\n";
-    } elsif ($uri =~ /goodbye/) {
-say_goodbye($r);
+    print "<hr>\n";
+    $parsed_uri = $r->parsed_uri();
+
+    print "s".$parsed_uri->scheme;print "<br>";
+    print "u".$parsed_uri->user;print "<br>";
+    print "pw".$parsed_uri->password;print "<br>";
+    print "h".$parsed_uri->hostname;print "<br>";
+    print "pt".$parsed_uri->port;print "<br>";
+    print "pa".$parsed_uri->path;print "<br>";
+    print "rpa".$parsed_uri->rpath;print "<br>";
+    print "q".$parsed_uri->query;print "<br>";
+    print "f".$parsed_uri->fragment;print "<br>";
+    print "<hr>\n";
+  
+    @uri_components = split("/", $uri);
+    foreach my $i (@uri_components) {
+      print "x:".$i;
     }
+    my $from_gp = $uri_components[2];
+    my $to_gp = $uri_components[3];
+    print "<h1>$from_gp $to_gp</h1><hr>";
+  }
 
      print "mod_perl 2.0 rocks!\n";
 
@@ -39,7 +66,7 @@ if (!$dbh) {
 }
 print "result\n";
 
-my $query = "select * from guidepost";# where id < 20";
+my $query = "select * from guidepost where id < 30";
 $res = $dbh->selectall_arrayref($query);
 
 print $DBI::errstr;
@@ -84,8 +111,6 @@ foreach my $row (@$res) {
 #   }
 
    $dbh->disconnect;
-   
-
-      return Apache2::Const::OK;
-  }
-  1;
+   return Apache2::Const::OK;
+}
+1;
